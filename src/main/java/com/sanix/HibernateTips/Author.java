3bb7d807c777225fbd7a82c9e3717355934a2039
sanix <sanicksikani@gmail.com>
2020-09-29 01:53:28 +0300

    Rabdom number-based UUID

--- src/main/java/com/sanix/HibernateTips/Author.java
+++ src/main/java/com/sanix/HibernateTips/Author.java
@@ -1,27 +1,13 @@
 package com.sanix.HibernateTips;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.*;
+import java.util.UUID;
 
 @Entity
 public class Author {
 
-    @Enumerated(EnumType.ORDINAL)
-    private AuthorStatus status;
-    //When you use this mapping to persist an Author
-    //entity with STATUS PUBLISHED
-    //Hibernate will store the value 0 in the database
-
-    //if you don't provide an @Enumerated annotation or don't
-    //set an EnumType as its value, Hibernate also uses the
-    //ordinal value as the default mapping
-
-    /*if you want to use String representation of the enum value
-    in the database you need to annotate the entity attribute with
-    @Enumerated and set EnumType.STRING as its value
-    When you persist the same entity in the database, Hibernate writes
-    the value PUBLISHED into the database column STATUS
-
-    */
+   @Id
+   @GeneratedValue
+   @Column(name="id", updatable=false, nullable=false)
+   private UUID id;
 }
